9f440f12ab8a10b9763274f18725da85c7ce4399
Bart Nijland <bartnijland91@gmail.com>
2018-11-20 14:16:29 +0100

    Lowercase search

--- library/src/main/java/nl/bartnijland/countrycodepickerdialog/utils/CountryCodesUtil.kt
+++ library/src/main/java/nl/bartnijland/countrycodepickerdialog/utils/CountryCodesUtil.kt
@@ -4,7 +4,7 @@ import nl.bartnijland.countrycodepickerdialog.models.CountryCode
 
 object CountryCodesUtil {
     fun filterCountryCodes(country: String, countryCodes: List<CountryCode>): List<CountryCode> {
-        return countryCodes.filter { it.name.startsWith(country) }
+        return countryCodes.filter { it.name.toLowerCase().startsWith(country.toLowerCase()) }
     }
     fun getPositionForCountry(countryCode: String, countryCodes: List<CountryCode>): Int {
         val countryCode= countryCodes.firstOrNull { it.code.toLowerCase() == countryCode.toLowerCase() }
